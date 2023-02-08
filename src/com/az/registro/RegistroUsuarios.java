7385a29700a2d6a90e5049b40ff3c3c50ed7a7c9
jaguilar <julio.aguilar@lania.edu.mx>
2023-02-08 12:17:46 -0600

    Version para pruebas

---
+++ src/com/az/registro/RegistroUsuarios.java
@@ -0,0 +1,17 @@
+package com.az.registro;
+
+/**
+ *
+ * @author jaguilar
+ */
+public class RegistroUsuarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaRegistro.main(args);
+    }
+    
+}
